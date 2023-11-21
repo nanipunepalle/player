fbf2eb73ca3aaa2b85f9618f15be3a48b3bd5153
hborawski <Harris_Borawski@intuit.com>
2023-11-21 12:22:17 -0800

    view inspector tests running

---
+++ ios/ViewInspector/reference-assets/ReferenceAssetsPluginTests.swift
@@ -0,0 +1,31 @@
+//
+//  ReferenceAssetsPluginTests.swift
+//  PlayerUI_Tests
+//
+//  Created by Borawski, Harris on 4/17/20.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import JavaScriptCore
+@testable import PlayerUI
+@testable import PlayerUISwiftUI
+@testable import PlayerUIReferenceAssets
+
+class SwiftUIReferenceAssetsPluginTests: XCTestCase {
+    func testReferenceAssetsPluginConstructs() {
+        let context = JSContext()!
+
+        let plugin = ReferenceAssetsPlugin()
+        plugin.context = context
+
+        XCTAssertNotNil(plugin.pluginRef)
+    }
+
+    func testReferenceAssetRegistration() {
+        let player = SwiftUIPlayer(flow: "", plugins: [ReferenceAssetsPlugin()])
+
+        XCTAssertEqual(player.assetRegistry.registeredAssets.count, 5)
+    }
+}
